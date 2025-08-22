450a37d94fce7426b7f231bf0b5ae078d0e141ec
Rustam <30387667+RustamZakAs@users.noreply.github.com>
2025-08-22 21:51:16 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.IO.Compression;
+using System.Threading;
+
+namespace TradeIntellect.Updater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string zipFile = args[0];
+            string appDir = args[1];
+            string exePath = args[2];
+
+            // ждём, пока основное приложение закроется
+            Thread.Sleep(2000);
+
+            // распаковываем архив в папку приложения
+            ZipFile.ExtractToDirectory(zipFile, appDir);
+
+            // запускаем новое приложение
+            Process.Start(exePath);
+        }
+    }
+}
